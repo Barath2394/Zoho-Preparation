09122c9db4e9f78c6600a68f95de3a3f688a0134
Barath <barathbarth128@gmail.com>
2024-12-24 19:53:56 +0530

    Always remember

--- Zoho-Qns/Zoho/PointToBeRemembered.java
+++ Zoho-Qns/Zoho/PointToBeRemembered.java
@@ -2,6 +2,6 @@ package Barath.com.Zoho;
 
 public class PointToBeRemembered {
     public static void main(String[] args) {
-        System.out.println("C.Barath SDE @Zoho");
+        System.out.println("Hard Work never fails");
     }
 }
